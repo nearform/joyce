7b7f53e3cbe1d6b017fb1098b163278452e3d861
Ryan Roemer <ryan.roemer@nearform.com>
2026-01-06 21:57:11 -0800

    More robust params handling

--- public/app/components/persistent-link.js
+++ public/app/components/persistent-link.js
@@ -1,10 +1,29 @@
+/* global URLSearchParams:false,URL:false */
 import { Link, useLocation } from "react-router";
 import { html } from "../util/html.js";
+
+// Merge current location search params into a destination path
+const mergeSearch = (to, currentSearch) => {
+  if (!currentSearch) return to;
+
+  // URL requires an absolute URL, so use a dummy base for relative paths
+  const url = new URL(to, "http://x");
+  const currentParams = new URLSearchParams(currentSearch);
+
+  // Add current params that aren't already in the destination
+  for (const [key, value] of currentParams) {
+    if (!url.searchParams.has(key)) {
+      url.searchParams.set(key, value);
+    }
+  }
+
+  return url.pathname + url.search + url.hash;
+};
 
 // Preserves ALL current search params when navigating
 export const PersistentLink = ({ to, children, ...props }) => {
   const location = useLocation();
-  const toWithSearch = location.search ? `${to}${location.search}` : to;
+  const toWithSearch = mergeSearch(to, location.search);
 
   return html`<${Link} to=${toWithSearch} ...${props}>${children}</${Link}>`;
 };
